8230f4868a31c3ee710b77e0659c7d113083c771
quimort <quimort97@gmail.com>
2022-10-29 14:14:48 +0200

    modifayed classic shelling vectorized

--- Schelling-model-of-racial-2 vectorized.py
+++ Schelling-model-of-racial-2 vectorized.py
@@ -2,12 +2,14 @@ import numpy as np
 from scipy.signal import convolve2d as convolve
 import time
 # Gloval variables of the simulation
-N = 60
+N = 20
 sim_t = 0.4
 empty = 0.1
 A_to_B = 1
 Kernel = np.array([[1,1,1],[1,0,1],[1,1,1]],dtype=np.int8)
-
+epsilon = 0.00001
+bloked = False
+blocks= np.array([False,False])
 def rand_init(N,empty,a_to_b):
     """ Random grid initialitzation
     A = 0
@@ -35,20 +37,24 @@ def evolve(M,boundary='wrap'):
     then the individual moves to an empty house. 
     """
     Kws = dict(mode='same',boundary=boundary)
+    global blocks 
+    global bloked 
     a_neights = convolve(M == 0,Kernel,**Kws)
     b_neights = convolve(M == 1,Kernel,**Kws)
     neights = convolve(M != -1,Kernel,**Kws)
-    a_dissatisfaction_1 = np.where(((M==0)&(neights != 0)) ,a_neights/neights,3)
-    print(((M==0)&(neights != 0)))
-    b_dissatisfaction_1 = np.where(((M==1)&(neights != 0)) ,a_neights/neights,3)
-    a_dissatisfaction = (a_dissatisfaction_1 < sim_t)
-    b_dissatisfaction = (b_dissatisfaction_1 < sim_t)
-    vacant_dissatisfaction = (neights == 0)&(M != -1)
-    dissatisfaction = a_dissatisfaction + b_dissatisfaction +vacant_dissatisfaction
-    cordenates = np.where(dissatisfaction == True)
+    a_neights = a_neights + epsilon
+    b_neights = b_neights + epsilon
+    neights = neights + epsilon
+    a_dissatisfaction = (a_neights/neights < sim_t)&(M == 0)
+    b_dissatisfaction = (b_neights/neights < sim_t)&(M == 1)
+    n_a_dissatisfied, n_b_dissatisfied = a_dissatisfaction.sum(),b_dissatisfaction.sum()
+    dissatisfaction_n = (n_a_dissatisfied+n_b_dissatisfied)/np.size(M)
+    cordenates_a = np.where(a_dissatisfaction == True)
+    cordenates_b = np.where(b_dissatisfaction == True)
+    cordenates = cordenates_a + cordenates_b
     index = np.vstack((cordenates[0], cordenates[1])).T
     if (np.size(index,axis=0) == 0):
-        return M
+        return M,dissatisfaction_n
     random_number = np.random.randint(np.size(index,axis=0),size=1)
     random_index = index[random_number][0]
     cordenates_vacant = np.where((M == -1) == True)
@@ -59,31 +65,74 @@ def evolve(M,boundary='wrap'):
     if (agent_tipe == 0 ):
         a_neights_vacants = a_neights[Y,X]
         neights_vacants = neights[Y,X]
-        satisfaying_vacants = (a_neights_vacants/neights_vacants >= sim_t)
-        array_of_good_vacants = np.where(satisfaying_vacants == True)
-        if(len(array_of_good_vacants[0]) != 0):
+        a_neights_vacants = a_neights_vacants + epsilon
+        neights_vacants = neights_vacants +epsilon
+        satisfaying_vacants_a = (a_neights_vacants/neights_vacants >= sim_t)
+        if(True in satisfaying_vacants_a):
+            array_of_good_vacants = np.where(satisfaying_vacants_a == True)
             move_to = index_vacants[array_of_good_vacants[0][0]]
             M[random_index[0]][random_index[1]] = -1
             M[move_to[0]][move_to[1]] = 0
-
+        if( True not in satisfaying_vacants_a):
+            blocks[0] = True
     else:
         b_neights_vacants = b_neights[Y,X]
         neights_vacants = neights[Y,X]
-        satisfaying_vacants = (b_neights_vacants/neights_vacants >= sim_t)
-        array_of_good_vacants = np.where(satisfaying_vacants == True)
-        if(len(array_of_good_vacants[0]) != 0):
+        b_neights_vacants = b_neights_vacants + epsilon
+        neights_vacants = neights_vacants +epsilon
+        satisfaying_vacants_b = (b_neights_vacants/neights_vacants >= sim_t)
+        if(True in satisfaying_vacants_b):
+            array_of_good_vacants = np.where(satisfaying_vacants_b == True)
             move_to = index_vacants[array_of_good_vacants[0][0]]
             M[random_index[0]][random_index[1]] = -1
             M[move_to[0]][move_to[1]] = 1
- 
-    
-    return M
+        if( True not in satisfaying_vacants_b):
+            blocks[1] = True
+    if(blocks[0] == True):
+        "a agents are blocked"
+        index_test = np.vstack((cordenates_b[0], cordenates_b[1])).T
+        cordenate_test = index_test[0]
+        b_neights_vacants = b_neights[Y,X]
+        neights_vacants = neights[Y,X]
+        b_neights_vacants = b_neights_vacants + epsilon
+        neights_vacants = neights_vacants +epsilon
+        satisfaying_vacants_b = (b_neights_vacants/neights_vacants >= sim_t)
+        if(True in satisfaying_vacants_b):
+            array_of_good_vacants = np.where(satisfaying_vacants_b == True)
+            move_to = index_vacants[array_of_good_vacants[0][0]]
+            M[cordenate_test[0]][cordenate_test[1]] = -1
+            M[move_to[0]][move_to[1]] = 1
+        if( True not in satisfaying_vacants_b):
+            blocks[1] = True
+    if(blocks[1] == True):
+        "b agents are blocked"
+        index_test = np.vstack((cordenates_a[0], cordenates_a[1])).T
+        cordenate_test = index_test[0][0]
+        a_neights_vacants = a_neights[Y,X]
+        neights_vacants = neights[Y,X]
+        a_neights_vacants = a_neights_vacants + epsilon
+        neights_vacants = neights_vacants +epsilon
+        satisfaying_vacants_a = (a_neights_vacants/neights_vacants >= sim_t)
+        if(True in satisfaying_vacants_a):
+            array_of_good_vacants = np.where(satisfaying_vacants_a == True)
+            move_to = index_vacants[array_of_good_vacants[0][0]]
+            M[cordenate_test[0]][cordenate_test[1]] = -1
+            M[move_to[0]][move_to[1]] = 1
+        if( True not in satisfaying_vacants_a):
+            blocks[0] = True
+    if(blocks[0] == True and blocks[1] == True):
+       bloked= True
+    print(dissatisfaction_n)
+    return M,dissatisfaction_n
 def get_mean_similarity_ratio(M,boundary='wrap'):
 
     Kws = dict(mode='same',boundary=boundary)
     a_neights = convolve(M == 0,Kernel,**Kws)
     b_neights = convolve(M == 1,Kernel,**Kws)
     neights = convolve(M != -1,Kernel,**Kws)
+    a_neights = a_neights + epsilon
+    b_neights = b_neights + epsilon
+    neights = neights + epsilon
     n_similar_a = np.where(np.logical_and(np.logical_and(M !=-1,M == 0),neights != 0),\
         a_neights/neights,0)
     n_similar_b = np.where(np.logical_and(np.logical_and(M !=-1,M == 1),neights != 0),\
@@ -97,11 +146,11 @@ def get_mean_dissatisfaction(M,boundary='wrap'):
     a_neights = convolve(M == 0,Kernel,**Kws)
     b_neights = convolve(M == 1,Kernel,**Kws)
     neights = convolve(M != -1,Kernel,**Kws)
-    a_dissatisfaction_1 = np.where(np.logical_and(M==0,neights != 0),a_neights/neights,3)
-    b_dissatisfaction_1 = np.where(np.logical_and(M==1,neights != 0),a_neights/neights,3)
-    a_dissatisfaction = (a_dissatisfaction_1 < sim_t)
-    b_dissatisfaction = (b_dissatisfaction_1 < sim_t)
-
+    a_neights = a_neights + epsilon
+    b_neights = b_neights + epsilon
+    neights = neights + epsilon
+    a_dissatisfaction = (a_neights/neights < sim_t)&(M == 0)
+    b_dissatisfaction = (b_neights/neights < sim_t)&(M == 1)
     n_a_dissatisfied, n_b_dissatisfied = a_dissatisfaction.sum(),b_dissatisfaction.sum()
 
     return (n_a_dissatisfied+n_b_dissatisfied)/np.size(M)
@@ -110,12 +159,11 @@ M = rand_init(N,empty,A_to_B)
 similarity = get_mean_similarity_ratio(M)
 dissatisfacton = get_mean_dissatisfaction(M)
 print("similarity initial = {} /dissatisfaction initial = {}".format(similarity,dissatisfacton))
-print((M==-1).sum())
-for i in range(51000):
-    M = evolve(M)
-    if (dissatisfacton == 0):
-        break
-print((M==-1).sum())
+continua = True
+while(continua):
+    M,dissatisfaction_n = evolve(M)
+    if (dissatisfaction_n == 0 or bloked == True):
+        continua = False
 similarity = get_mean_similarity_ratio(M)
 dissatisfacton = get_mean_dissatisfaction(M)
 print("similarity final = {} /dissatisfaction final = {}".format(similarity,dissatisfacton))
